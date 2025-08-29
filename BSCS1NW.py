1a0ed47261c68ff24975554cc19c672ed3d3b4b6
meowkittyyah <105548899+mjbmw@users.noreply.github.com>
2025-08-29 12:26:02 +0800

    Add files via upload

---
+++ BSCS1NW.py
@@ -0,0 +1,26 @@
+print("State your Information")
+
+user_respose= input("Are you Student? (yes/no): ")
+if user_respose.lower()== "yes":
+    print("The ticket is P200")
+elif user_respose.lower() == "no":
+    print("Stoping the operation.")
+
+user_respose = input ("What's your Name?")
+
+user_respose = input ("What's your Age?")
+if user_respose.lower()== "13":
+    print("The ticket is P100.")
+if user_respose.lower()== "17":
+    print("The ticket is P120.")
+if user_respose.lower() == "18":
+    print("The ticket is P150.")
+if user_respose.lower() == "59":
+    print("The ticket is P150.")
+if user_respose.lower()== "60":
+    print("The ticket is P80.")
+                      
+
+
+ 
+      
