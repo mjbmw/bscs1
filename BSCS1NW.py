7bdd46cab23ac02637abf7d18ee1409a9b4335f6
meowkittyyah <105548899+mjbmw@users.noreply.github.com>
2025-08-30 14:22:42 +0800

    sirarvin.pt

--- BSCS1NW.py
+++ BSCS1NW.py
@@ -1,26 +1,34 @@
 print("State your Information")
 
-user_respose= input("Are you Student? (yes/no): ")
-if user_respose.lower()== "yes":
-    print("The ticket is P200")
-elif user_respose.lower() == "no":
-    print("Stoping the operation.")
+name = input("Whats your name?")
+age = int(input("What's your age?"))
+student = input("Are you student? (yes/no): ").lower()
 
-user_respose = input ("What's your Name?")
+if age < 13:
+    price = 100
 
-user_respose = input ("What's your Age?")
-if user_respose.lower()== "13":
-    print("The ticket is P100.")
-if user_respose.lower()== "17":
-    print("The ticket is P120.")
-if user_respose.lower() == "18":
-    print("The ticket is P150.")
-if user_respose.lower() == "59":
-    print("The ticket is P150.")
-if user_respose.lower()== "60":
-    print("The ticket is P80.")
-                      
+elif 13 <= age <= 17 and student == "yes":
+    price = 120
+
+elif 18 <= age <=59:
+    if student == "yes":
+        price = 150
+    else:
+        price = 200
+
+elif age >=60:
+    price = 80
+
+else:
+    price = 0
+
+print("Ticket Information")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Student: {student}")
+print(f"Ticket Price: P{price}")
 
 
  
       
+
